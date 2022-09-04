1739f0460b1be807e3481e85c27df9c2bbb55b41
AsifBashir <56689372+Asif26@users.noreply.github.com>
2022-09-05 03:40:40 +0500

    Update practics_09_05_2022.sql
    updated

--- practics_09_05_2022.sql
+++ practics_09_05_2022.sql
@@ -7,3 +7,16 @@ select * from bank_details limit 10;
 select * from bank_details where age = 50 or age = 60;
 select * from bank_details where age  = 50 ;
 select * from bank_details where age = 60 and job = 'retired' and marital = 'single';
+select * from bank_details where education = 'unknown' and marital = 'single' and balance < 500;
+select * from bank_details where (education = 'unknown' or marital = 'single') and balance < 500;
+select distinct job from bank_details;
+select * from bank_details;
+select * from bank_details order by age;
+select * from bank_details order by age desc;
+select sum(balance) from bank_details;
+select avg(balance) from bank_details;
+select count(balance) from bank_details;
+
+select distinct count(job) from bank_details;
+select min(balance) from bank_details;
+select max(balance) from bank_details;
